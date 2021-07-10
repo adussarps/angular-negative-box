c5d99fcf890686284b487b803fa967ac7a1d4818
adussarps <antoine.dussarps@datadoghq.com>
2021-07-10 21:49:00 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SenderFormComponent } from './sender-form/sender-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FileUploaderComponent } from './file-uploader/file-uploader.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [AppComponent, HelloComponent, SenderFormComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, FileUploadModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SenderFormComponent,
+    FileUploaderComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/file-uploader/file-uploader.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { FileUploader } from 'ng2-file-upload';
+
+// const URL = '/api/';
+const URL = 'api';
+
+@Component({
+  selector: 'app-file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.css']
+})
+export class FileUploaderComponent {
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
+  hasAnotherDropZoneOver: boolean;
+  response: string;
+  file: File;
+
+  constructor() {
+    this.uploader = new FileUploader({
+      url: URL,
+      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      formatDataFunctionIsAsync: true,
+      formatDataFunction: async item => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            name: item._file.name,
+            length: item._file.size,
+            contentType: item._file.type,
+            date: new Date()
+          });
+        });
+      }
+    });
+
+    this.hasBaseDropZoneOver = false;
+    this.hasAnotherDropZoneOver = false;
+
+    this.response = '';
+
+    this.uploader.response.subscribe(res => (this.response = res));
+  }
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e: any): void {
+    this.hasAnotherDropZoneOver = e;
+  }
+}
